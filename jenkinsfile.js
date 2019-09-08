b51597726b1b4a8c38711afadcdf543051eae9b1
Cesar Luna <cesar.luna@hotmail.com>
2019-09-07 18:15:56 -0700

    update jenkins

--- jenkinsfile.js
+++ jenkinsfile.js
@@ -1,10 +1,9 @@
-Jenkinsfile (Declarative Pipeline)
 pipeline {
-    agent { docker { image 'node:6.3' } }
+    agent any
     stages {
         stage('build') {
             steps {
-                sh 'npm --version'
+                echo 'Hello World'
             }
         }
     }
